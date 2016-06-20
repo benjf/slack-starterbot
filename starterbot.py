d045b4a3bd6d9e7fea9308e26a92c7f685b49f7b
benjf <benj.fredrick@gmail.com>
2016-06-20 13:09:08 -0600

    create the cat facts bot, based on the starterbot tutorial

--- starterbot.py
+++ starterbot.py
@@ -1,57 +1,85 @@
+import json
 import os
 import time
+import urllib2
+
 from slackclient import SlackClient
 
 
-# starterbot's ID as an environment variable
-#BOT_ID = os.environ.get("BOT_ID")
-
 # constants
-TRIGGER = "cat"
-TARGET = "rmoellering"
+TRIGGER = os.environ.get('CATFACT_BOT_TRIGGER_STRING')
+USER_ID = os.environ.get('CATFACT_BOT_TARGET_USER_ID')
+BOT_ID = os.environ.get('CATFACT_BOT_ID')
 
 # instantiate Slack client
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 
-def handle_command(command, channel):
-    """
-        Receives commands directed at the bot and determines if they
-        are valid commands. If so, then acts on the commands. If not,
-        returns back what it needs for clarification.
-    """
-    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
-               "* command with numbers, delimited by spaces."
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
-    slack_client.api_call("chat.postMessage", channel=channel,
-                          text=response, as_user=True)
-
-
 def parse_slack_output(slack_rtm_output):
     """
-        The Slack Real Time Messaging API is an events firehose.
-        this parsing function returns None unless a message is
-        directed at the Bot, based on its ID.
+    The Slack Real Time Messaging API is an events firehose.
+    This parsing function scans the content for the TRIGGER string (also not sent by the Bot itself),
+    and if found, calls the catfacts API.
     """
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
-            if output and 'text' in output and AT_BOT in output['text']:
-                # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
-                       output['channel']
-    return None, None
+            if output and 'text' in output and TRIGGER in output['text'] and BOT_ID != output['user']:
+                print "getting cat fact ..."
+                return get_cat_fact()
 
+    return None
+
+
+def get_cat_fact():
+    """
+    Hit the CatFacts API and get some cat knowledge
+    :return: String
+    """
+
+    url = 'http://catfacts-api.appspot.com/api/facts?number=1'
+    response = json.load(urllib2.urlopen(url))
+
+    if response['success'] == "true":
+        return response['facts'][0]
+
+    else:
+        return 'Kitty loves you'
+
+
+def send_cat_fact(cat_fact):
+
+    # first, we open the IM channel
+    result = slack_client.api_call("im.open", user=USER_ID)
+
+    if 'ok' in result and result['ok'] == 'success':
+        print 'error opening the IM channel'
+        return
+
+    else:
+        channel_id = result['channel']['id']
+
+        # then we send the message
+        slack_client.api_call("chat.postMessage", channel=channel_id, text=cat_fact, as_user=True)
 
 if __name__ == "__main__":
-    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+
+    if not TRIGGER or not USER_ID or not BOT_ID:
+        print("One or more required environment variables are missing.")
+        print("CATFACT_BOT_TRIGGER_STRING: {}".format(TRIGGER))
+        print("CATFACT_BOT_TARGET_USER_ID: {}".format(USER_ID))
+        print("CATFACT_BOT_ID: {}".format(BOT_ID))
+        exit()
+
+    READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
         print("StarterBot connected and running!")
         while True:
-            command, channel = parse_slack_output(slack_client.rtm_read())
-            if command and channel:
-                handle_command(command, channel)
+            output = parse_slack_output(slack_client.rtm_read())
+            if output:
+                print output
+                send_cat_fact(output)
+
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
-        print("Connection failed. Invalid Slack token or bot ID?")
+        print("Connection failed. Invalid Slack token?")
